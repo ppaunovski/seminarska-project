102dd03f729b54da2b9c325535f09abea5a2a0e5
ppaunovski <paunovskipavel@gmail.com>
2022-09-18 21:38:31 +0200

    Updates

---
+++ precache-manifest.b8282d95aa92902816a07102287505b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "89331ab5eaceb6c7ae8b",
+    "url": "/seminarska-project/static/css/main.a7e59400.chunk.css"
+  },
+  {
+    "revision": "89331ab5eaceb6c7ae8b",
+    "url": "/seminarska-project/static/js/main.89331ab5.chunk.js"
+  },
+  {
+    "revision": "28b8e9e42cf16559ecdf",
+    "url": "/seminarska-project/static/js/1.28b8e9e4.chunk.js"
+  },
+  {
+    "revision": "7e89302e516bd62cd99a",
+    "url": "/seminarska-project/static/css/2.cefbe923.chunk.css"
+  },
+  {
+    "revision": "7e89302e516bd62cd99a",
+    "url": "/seminarska-project/static/js/2.7e89302e.chunk.js"
+  },
+  {
+    "revision": "2c4a377dbaff8c1fd6d8",
+    "url": "/seminarska-project/static/js/runtime~main.2c4a377d.js"
+  },
+  {
+    "revision": "01e846d9ba85abaee10d0c2fc244c275",
+    "url": "/seminarska-project/index.html"
+  }
+];
