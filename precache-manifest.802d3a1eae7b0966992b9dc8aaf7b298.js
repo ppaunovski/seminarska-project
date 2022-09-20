a3c94b255b72540522fdb6004c9578ef6d2710b4
ppaunovski <paunovskipavel@gmail.com>
2022-09-20 19:49:36 +0200

    Updates

---
+++ precache-manifest.802d3a1eae7b0966992b9dc8aaf7b298.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "63d90f1cfa70000f197d",
+    "url": "/seminarska-project/static/css/main.a7e59400.chunk.css"
+  },
+  {
+    "revision": "63d90f1cfa70000f197d",
+    "url": "/seminarska-project/static/js/main.63d90f1c.chunk.js"
+  },
+  {
+    "revision": "28b8e9e42cf16559ecdf",
+    "url": "/seminarska-project/static/js/1.28b8e9e4.chunk.js"
+  },
+  {
+    "revision": "a5d356dcd0547e21cc39",
+    "url": "/seminarska-project/static/css/2.cefbe923.chunk.css"
+  },
+  {
+    "revision": "a5d356dcd0547e21cc39",
+    "url": "/seminarska-project/static/js/2.a5d356dc.chunk.js"
+  },
+  {
+    "revision": "2c4a377dbaff8c1fd6d8",
+    "url": "/seminarska-project/static/js/runtime~main.2c4a377d.js"
+  },
+  {
+    "revision": "f5036957a679de31c96bd78f82d65c7b",
+    "url": "/seminarska-project/index.html"
+  }
+];
--- src/components/LoggedInRoute.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext";
-
-export default function LoggedInRoute({ children }) {
-  const { currentUser } = useAuth();
-  return currentUser ? <Navigate to="/" /> : children;
-}
